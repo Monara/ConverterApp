b7656fd36c050471562f86a50d4ed1bca174038d
Ngoc Bui <44022931+ngocbu@users.noreply.github.com>
2018-11-30 13:47:44 +0200

    layout version 1 for unit converse length

---
+++ app/src/main/java/com/example/asus/converterapp/UnitCatogory.java
@@ -0,0 +1,13 @@
+package com.example.asus.converterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnitCatogory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.length);
+    }
+}

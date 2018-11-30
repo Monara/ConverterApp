594c3173397b5642be985f111a6857eea5009af9
Si Yi <siy@metropolia.fi>
2018-11-30 13:05:46 +0200

    this is the activity for Length.

---
+++ app/src/main/java/com/example/asus/converterapp/Length.java
@@ -0,0 +1,13 @@
+package com.example.asus.converterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Length extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_length);
+    }
+}

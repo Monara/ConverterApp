e7f19d178511ce56f17e8c8251f24664331e7140
Monika Radaviciute <monikar@metropolia.fi>
2018-11-30 10:50:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/asus/converterapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.converterapp;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+

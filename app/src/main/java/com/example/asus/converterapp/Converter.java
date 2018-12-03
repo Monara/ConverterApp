4fe8f13ff0ee6f017b3e5adf145c46b29804fc8e
Monika Radaviciute <monikar@metropolia.fi>
2018-12-03 22:04:05 +0200

    Starting with Length.class

---
+++ app/src/main/java/com/example/asus/converterapp/Converter.java
@@ -0,0 +1,11 @@
+package com.example.asus.converterapp;
+
+public class Converter {
+
+
+    public Converter (String fromUnit, String toUnit, double insertedNumber) {
+
+
+
+    }
+}
--- app/src/main/java/com/example/asus/converterapp/Length.java
+++ app/src/main/java/com/example/asus/converterapp/Length.java
@@ -3,6 +3,7 @@ package com.example.asus.converterapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -19,16 +20,65 @@ public class Length extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.length);
 
-        EditText inputBox = (EditText) findViewById(R.id.inputBox);
+        EditText inputBox = findViewById(R.id.inputBox);
+        TextView outputBox = findViewById(R.id.outputBox);
+        ImageButton buttonRevert = findViewById(R.id.buttonRevert);
+        ImageButton buttonConvert = findViewById(R.id.buttonConvert);
 
-        TextView outputBox = (TextView) findViewById(R.id.outputBox);
+        //read the value of drop down, define unitFrom and unitTo
+        //needs revision from here
+
+        double insertedNumber = Double.valueOf(inputBox.getText().toString());
+        Spinner unitFromSpinner = findViewById(R.id.unitFrom);
+       // int iUnitFrom = unitFromSpinner.getSelectedItemPosition();
+        Spinner unitToSpinner = findViewById(R.id.unitTo);
+        //int iUnitTo = unitToSpinner.getSelectedItemPosition();
 
 
-        ImageButton buttonRevert = (ImageButton) findViewById(R.id.buttonRevert);
+        unitFromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //nebaigtas
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int iUnitFrom = i;
+            }
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }});
 
-        ImageButton buttonConvert = (ImageButton) findViewById(R.id.buttonConvert);
+        unitToSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //nebaigtas
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               int iUnitTo = i;
+            }
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }});
 
-    }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Converter lengthConverter = new Converter (insertedNumber) { //add spinner values
+
+
+
+
+
+
 }
 
 
+}

d4c18c75e667beae63c647ab3761950b2d07e118
Si Yi <siy@metropolia.fi>
2018-12-04 10:03:06 +0200

    safest version 4/12/2018

--- app/src/main/java/com/example/asus/converterapp/Favorite.java
+++ app/src/main/java/com/example/asus/converterapp/Favorite.java
@@ -11,5 +11,5 @@ public class Favorite extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
     }
-    ListView listView=findViewById(R.id.list_view);
+
 }
